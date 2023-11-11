89dde19250e1aac56fb84396800e46b8a82f28b4
Abdul Wahed Sabury <sabury2020@gmail.com>
2023-11-11 10:18:05 +0200

    fake server for fetching data created

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,36 @@ import Product from "./pages/Product";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import "./index.css";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const BASE_URL = "http://localhost:9000";
+
+  useEffect(function(){
+    async function fetchCities (){
+      try{
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json();
+        setCities(data);
+      }catch{
+        alert("error on loading data")
+      }finally{
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  },[])
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List</p>} />
-          <Route path="cites" element={<p>List of Cites</p>} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cites" element={<CityList cities={cities} isLoading={isLoading}  />} />
           <Route path="countries" element={<p>List of countries</p>} />
         </Route>
         <Route path="login" element={<Login />} />
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,19 @@
+import styles from "./CityItem.module.css";
+export default function CityItem({ city }) {
+  const { cityName, date, emoji } = city;
+  const formatDate = (date) =>
+    new Intl.DateTimeFormat("en", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      weekday: "long",
+    }).format(new Date(date));
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)})</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
+}
